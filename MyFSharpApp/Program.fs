95b6233c9a696f28abd09edae079fc2e8f9817b0
gurleen0109 <gurleen.kaurhf6@triosstudent.com>
2023-11-29 09:02:11 -0700

    vers1

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,16 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// Define the discriminated union for the genre
+
+type Genre = 
+        | Horror 
+        | Drama 
+        | Thriller
+        | Comedy 
+        | Fantasy 
+        | Sport
+
+// Define the "Director" record type
+type Directrr = {
+  Name: string
+  Movies: number
+}
