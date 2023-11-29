969ba5d523bac2783c2864156642bd3f201db27a
gurleen0109 <gurleen.kaurhf6@triosstudent.com>
2023-11-29 09:39:36 -0700

    vers1

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -12,5 +12,25 @@ type Genre =
 // Define the "Director" record type
 type Directrr = {
   Name: string
-  Movies: number
+  Movies: int
 }
+
+// Define the "Movie" record type
+type Movie = {
+  Name: string
+  RunLength: int
+  Genre: Genre
+  Directrr: Directrr
+  IMDBRating: float
+}
+
+// Create movie instances
+
+let movie1 = { Name = "CODA"; RunLength = 111; Genre = Drama; Directrr = Sian Heder; IMDBRating = 8.1 }
+let movie2 = { Name = "Belfast"; RunLength = 98; Genre = Comedy; Directrr = Kenneth Branagh; IMDBRating = 7.3 }
+let movie3 = { Name = "Don't Look Up"; RunLength = 138; Genre = Comedy; Directrr = Adam Mckay; IMDBRating = 7.2 }
+let movie4 = { Name = "Dune"; RunLength = 155; Genre = Fantasy; Directrr = Denis Villeneuve; IMDBRating = 8.1 }
+let movie4 = { Name = "Drive My Car"; RunLength = 179; Genre = Drama; Directrr = Ryusuke Hamaguchi; IMDBRating = 7.6 }
+let movie4 = { Name = "King Richard"; RunLength = 144; Genre = Sport; Directrr = Reinaldo Marcus Green; IMDBRating = 7.5 }
+let movie4 = { Name = "Licorice Pizza"; RunLength = 133; Genre = Comedy; Directrr = Paul Thomas Anderson; IMDBRating = 7.4 }
+let movie4 = { Name = "Nightmare Alley"; RunLength = 150; Genre = Thriller; Directrr = Guillermo Del Toro ; IMDBRating = 7.1 }
